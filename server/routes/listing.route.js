2ee4881008424218f307b6ece7bc33ec83e06889
sumbareakshay <sumbareakshay@gmail.com>
2024-08-22 20:37:52 +0530

    complete list page frontend functionality

--- server/routes/listing.route.js
+++ server/routes/listing.route.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const listController = require("../controllers/listing.controller");
-// const verifyToken = require('../utils/verifyToken');
+ const verifyToken = require('../utils/verifyToken');
 
-router.post("/create", listController.createListing);
+router.post("/create",verifyToken, listController.createListing);
 
 module.exports = router;
