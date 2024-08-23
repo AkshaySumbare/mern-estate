5ee4d8b74f4d4d827b0eb17df188d5303a72f5d8
sumbareakshay <sumbareakshay@gmail.com>
2024-08-23 18:00:58 +0530

    Complete delete user listing

--- server/routes/listing.route.js
+++ server/routes/listing.route.js
@@ -4,5 +4,6 @@ const listController = require("../controllers/listing.controller");
  const verifyToken = require('../utils/verifyToken');
 
 router.post("/create",verifyToken, listController.createListing);
+router.delete("/delete/:id",verifyToken, listController.deleteListing);
 
 module.exports = router;
