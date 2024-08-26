2b6961a08901d6beff53b6bbd001cda6765f575d
sumbareakshay <sumbareakshay@gmail.com>
2024-08-26 21:16:47 +0530

    update 10

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ mongoose
     console.log(err);
   });
 
-  const __dirname = path.resolve();
+  const dirname = path.resolve();
 
 
 const app = express();
@@ -32,9 +32,9 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(dirname, '/client/dist')));
 app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+    res.sendFile(path.join(dirname, 'client', 'dist', 'index.html'))
 });
 
 
